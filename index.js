4b10b6e256ef91af8b13ab0da0210f93b152b425
Jay Parmar <jay.parmar@biztechcs.lan>
2023-06-06 17:04:11 +0530

    Configure store

---
+++ index.js
@@ -0,0 +1,15 @@
+const store = require('./app/store');
+const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state', store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(5));
+
+unsubscribe();
