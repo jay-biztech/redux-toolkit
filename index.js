1a0206e8c25f67540076f9e10a3d6e675acfc729
Jay Parmar <jay.parmar@biztechcs.lan>
2023-06-06 17:12:45 +0530

    Icecream feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const icecreamActions =
+  require('./features/icecream/icecreamSlice').icecreamActions;
 
 console.log('Initial state', store.getState());
 
@@ -11,5 +13,10 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(5));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(10));
 
 unsubscribe();
