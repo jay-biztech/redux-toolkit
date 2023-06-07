05fe8d245625f238cadda8fd972e681bc978f9fa
Jay Parmar <jay.parmar@biztechcs.lan>
2023-06-07 15:40:58 +0530

    Async thunks

--- app/store.js
+++ app/store.js
@@ -2,6 +2,7 @@ const configureStore = require('@reduxjs/toolkit').configureStore;
 const reduxLogger = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
+const userReducer = require('../features/user/userSlice');
 
 const logger = reduxLogger.createLogger();
 
@@ -9,6 +10,7 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
